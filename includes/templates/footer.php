06ada5bc6bbb681532702ec9d46e71e9a80054dc
Krohz <thealvias16@gmail.com>
2022-10-12 15:41:59 -0500

    1210-1

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -15,6 +15,6 @@
         <p class="copyright">Todos los derechos Reservados AC <?php echo $fecha?> &copy</p>
     </footer>
 
-    <script src="build/js/bundle.min.js"></script>
+    <script src="/build/js/bundle.min.js"></script>
 </body>
 </html>
