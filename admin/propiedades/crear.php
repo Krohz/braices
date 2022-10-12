3ec457c09e937e61d43359ae5ff356e0eae35ea4
Krohz <thealvias16@gmail.com>
2022-10-12 16:51:03 -0500

    1210-2

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,6 +7,45 @@
         <h1>Crear</h1>
 
         <a href="/admin" class="boton boton-verde">Volver</a>
+
+        <form action="" class="formulario">
+            <fieldset>
+                <legend>Informacion General</legend>
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" placeholder="Titulo Propiedad">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" placeholder="Precio Propiedad">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripcion:</label>
+                <textarea name="" id="descripcion"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información Propiedad</legend>
+                <label for="habitaciones">habitaciones:</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="estacionamiento">estacionamiento:</label>
+                <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+                <select name="" id="">
+                    <option value="1">Cristian</option>
+                    <option value="2">Karen</option>
+                </select>
+            </fieldset>
+
+            <input type="submit" value="Crear propiedad" class="boton boton-verde">
+        </form>
     </main>
 
 <?php 
