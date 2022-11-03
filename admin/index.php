04c2b87280646099e1a359eb9312102b563c233b
Krohz <thealvias16@gmail.com>
2022-11-03 14:24:00 -0500

    Read f

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,19 @@
 <?php 
 
+    // Importar la conexion
+    require '../includes/config/database.php';
+    $db = conectarDB();
+    // Escribir el query
+    $query = "SELECT * FROM propiedades";
+    // Consultar la BD
+    $resultadoConsulta = mysqli_query($db, $query);
+
+
+
+    // Muestra mensaje
     $resultado = $_GET['resultado'] ?? null;
 
+    //Incluye un template
     require '../includes/funciones.php';
     incluirTemplate('header');
     ?>
@@ -12,8 +24,39 @@
             <p class="alerta exito">Anuncio creado correctamente</p>
         <?php endif; ?>
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody><!--Mostrar los resultados-->
+                <?php while($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
+                <tr>
+                    <td> <?php echo $propiedad['id']; ?> </td>
+                    <td> <?php echo $propiedad['titulo']; ?> </td>
+                    <td><img src="/imagenes/<?php echo $propiedad['imagen']; ?> " class="imagen-tabla"></td>
+                    <td>$ <?php echo $propiedad['precio']; ?></td>
+                    <td>
+                        <a href="#" class ="boton-rojo-block">Eliminar</a>
+                        <a href="#" class ="boton-amarillo-block">Actualizar</a>
+                    </td>
+                    <?php endwhile; ?>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php 
+
+    // Cerra la conexion - OPCIONAL
+    mysqli_close($db);
+
     incluirTemplate('footer');
 ?>
